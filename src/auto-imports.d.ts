cb186d7827b0000eff2df1354440d8b5c437d1a9
jic999 <jic999@qq.com>
2023-05-03 20:11:31 +0800

    feat: ✨ 新增pinia示例

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -153,6 +153,7 @@ declare global {
   const useColorMode: typeof import('@vueuse/core')['useColorMode']
   const useConfirmDialog: typeof import('@vueuse/core')['useConfirmDialog']
   const useCounter: typeof import('@vueuse/core')['useCounter']
+  const useCounterStore: typeof import('./stores/counter')['useCounterStore']
   const useCssModule: typeof import('vue')['useCssModule']
   const useCssVar: typeof import('@vueuse/core')['useCssVar']
   const useCssVars: typeof import('vue')['useCssVars']
@@ -459,6 +460,7 @@ declare module 'vue' {
     readonly useColorMode: UnwrapRef<typeof import('@vueuse/core')['useColorMode']>
     readonly useConfirmDialog: UnwrapRef<typeof import('@vueuse/core')['useConfirmDialog']>
     readonly useCounter: UnwrapRef<typeof import('@vueuse/core')['useCounter']>
+    readonly useCounterStore: UnwrapRef<typeof import('./stores/counter')['useCounterStore']>
     readonly useCssModule: UnwrapRef<typeof import('vue')['useCssModule']>
     readonly useCssVar: UnwrapRef<typeof import('@vueuse/core')['useCssVar']>
     readonly useCssVars: UnwrapRef<typeof import('vue')['useCssVars']>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,8 @@
 defineOptions({
   name: 'IndexPage',
 })
+
+const counterStore = useCounterStore()
 </script>
 
 <template>
@@ -12,5 +14,16 @@ defineOptions({
     <p mt-24 text="14 gray-5" italic>
       Sprout is tiny, but it nurtures infinite hope.
     </p>
+    <div flex-center gap-x-12>
+      <n-button type="error" secondary circle @click="counterStore.decrease">
+        -
+      </n-button>
+      <div text-20>
+        {{ counterStore.count }}
+      </div>
+      <n-button type="primary" secondary circle @click="counterStore.increase">
+        +
+      </n-button>
+    </div>
   </div>
 </template>
